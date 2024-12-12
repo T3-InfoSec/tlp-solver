8c7b15cf5398f46173eab34cccf69ab6c3821247
c0d3sw0t <codeswot@proton.me>
2024-12-12 14:44:45 +0100

    enc hide to env

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
 
-
-class Settings(BaseSettings):
-    ENCRYPTION_KEY_PASSWORD: str = "vh9qFH0T9hzlhF++Z8aQjuMi++JCS2cPXZRnlL3/Jk4="
+class Settings(BaseSettings):    
     WEBSOCKET_URL: str = "ws://localhost:8000/ws/solver"
     PING_INTERVAL: int = 30  # seconds
+    ENCRYPTION_KEY_PASSWORD: str
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
