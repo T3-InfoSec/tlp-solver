6e7e2403eca8a291f1cd75560f0ec30461e9f97a
c0d3sw0t <codeswot@proton.me>
2024-12-18 17:59:39 +0100

    use gcm mode

--- app/utils/encryption.py
+++ app/utils/encryption.py
@@ -13,7 +13,7 @@ class Encryption:
         self.key = hashlib.sha256(password).digest()
 
     def encrypt(self, plaintext):
-        cipher = AES.new(self.key, AES.MODE_CBC)
+        cipher = AES.new(self.key, AES.MODE_GCM)
         iv = cipher.iv
         ciphertext = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
         # Combine IV and ciphertext, encode to base64
@@ -23,5 +23,5 @@ class Encryption:
         raw = base64.b64decode(encrypted_text)
         iv = raw[:AES.block_size]
         ciphertext = raw[AES.block_size:]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        cipher = AES.new(self.key, AES.MODE_GCM, iv)
         return unpad(cipher.decrypt(ciphertext), AES.block_size).decode()
